1df8b25b3de10705ef1515f6221d79a6213d59c2
Ayush Pratap Singh <161467009+Ayuxh-Pratap@users.noreply.github.com>
2024-03-09 10:41:46 +0530

    com ban btn fix res

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -6,9 +6,11 @@ const Banner = () => {
       <h2 className='primary font-medium text-[1.8rem] mb-6'>Join us</h2>
       <h3 className='lg:w-[40vw] w-auto leading-10 mb-8'>Join Us by Following Our Socials or Drop a Message</h3>
       <p className='lg:w-[40vw] w-auto mb-6'>Install our top-rated dropshipping app to your e-commerce site and get access to US Suppliers, AliExpress vendors, and the best dropshipping and custom products.</p>
-      <button className='btn primary mr-2'><a href="/">Follow Us</a></button>
-      <button className='btn primary ml-2'><a href="/contact">Send a Hi</a></button>
-    </section>
+      <div className='flex justify-center gap-4'>
+        <button className='btn'><a href="/courseDetail" className='text-[0.8rem]'>Follow Us</a></button>
+        <button className='btn'><a href="/" className='text-[0.8rem]'>Send a Hi!</a></button>
+      </div>
+    </section >
   )
 }
 
